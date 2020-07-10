2947eec9cab9de0c6f6eed3907315baa5c10d435
Alvaro Frias <alvarofriasgaray@gmail.com>
2020-07-10 20:52:01 -0300

    *Added semantic equation for Or, Lt operations *renamed file Added more examples

--- Lab2/Lab2.hs
+++ Lab2/Lab2.hs
@@ -22,7 +22,9 @@ data Expr a where
   -- logical operators
   And  :: Expr MBool -> Expr MBool -> Expr MBool
   Not  :: Expr MBool -> Expr MBool
-  Comp :: Expr MInt -> Expr MInt -> Expr MBool
+  Eq :: Expr MInt -> Expr MInt -> Expr MBool
+  Lt :: Expr MInt -> Expr MInt -> Expr MBool
+  Or :: Expr MBool -> Expr MBool -> Expr MBool
 
 
 {- Funciones semánticas -}
@@ -32,40 +34,39 @@ class DomSem dom where
 instance DomSem MInt where
   sem (CVar a) state = Just (state a)
   sem (CInt a) state = Just a
-  sem (Plus a b) state = parse_bin_ops (+) (sem a state) (sem b state)
-  sem (Prod a b) state = parse_bin_ops (*) (sem a state) (sem b state)
-  sem (Divs a b)  state= if (sem b state) == Just 0 then Nothing else parse_bin_ops (div) (sem a state) (sem b state)
-  sem (Op a)  state = parse_bin_ops (-) (Just 0) (sem a state) 
+  sem (Plus a b) state = ((+)-^-) (sem a state) (sem b state)
+  sem (Prod a b) state = ((*)-^-) (sem a state) (sem b state)
+  sem (Divs a b)  state= if (sem b state) == Just 0 then Nothing else ((div)-^-) (sem a state) (sem b state)
+  sem (Op a)  state =  ((-)-^-) (Just 0) (sem a state) 
 
 
 instance DomSem MBool where
   sem (CBool a) state = Just a
-  sem (And a b) state= parse_bin_ops (&&) (sem a state) (sem b state)
-  sem (Not a) state = parse_un_ops (not) (sem a state) 
-  sem (Comp a b) state = parse_bin_ops (==) (sem a state) (sem b state)
-
+  sem (And a b) state= ((&&)-^-) (sem a state) (sem b state)
+  sem (Not a) state =  ((not)-^) (sem a state) 
+  sem (Eq a b) state = ((==)-^-) (sem a state) (sem b state)
+  sem (Lt a b) state = ((<)-^-) (sem a state) (sem b state)
+  sem (Or a b) state = ((||)-^-) (sem a state) (sem b state)
 
 
 {- Funciones auxiliares -}
---(-^-) :: (a -> b -> c) -> (Maybe a -> Maybe b -> Maybe c)
---(-^-) = liftA2
+(-^-) :: (a -> b -> c) -> (Maybe a -> Maybe b -> Maybe c)
+(-^-) = liftA2
 
---(-^) :: (a -> b) -> (Maybe a -> Maybe b)
---(-^) = liftA
+(-^) :: (a -> b) -> (Maybe a -> Maybe b)
+(-^) = liftA
+
+
+-- ejemplos
 
 state :: Σ
 state s | s == "z" = 12
         | s == "x" = 0
 
 
-parse_bin_ops :: (a->b->c) -> Maybe a -> Maybe b -> Maybe c
-parse_bin_ops f a b = liftA2 (f) (a) (b)
-
-parse_un_ops :: (a->b) -> Maybe a -> Maybe b 
-parse_un_ops f a = liftA (f) (a)
-
-
 -- ejemplos --
 -- sem (Divs (CInt 2) (Plus (CVar "x") (CInt  0))) state == Nothing
 -- sem (Divs (Op (Prod (CInt 2) (CInt 2))) (Plus (CInt 1) (CInt 1))) state == Just (-2) 
--- sem (Comp (Divs (Op (Prod (CInt 2) (CInt 2))) (Plus (CInt 1) (CInt 1))) (CVar "x")) state == Just False
+-- sem (Eq (Divs (Op (Prod (CInt 2) (CInt 2))) (Plus (CInt 1) (CInt 1))) (CVar "x")) state == Just False
+-- sem (And (Or (CBool False) (Lt (CInt 0) (CInt 2))) (Eq (CVar "x") (CInt 0))) == Just True
+-- sem ( Eq (Divs (CInt 1) (CVar "x")) (CInt 1)) state == Nothing
