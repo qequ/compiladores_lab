4dd31296778dac39c14a255e1214aaaf85b2c4f0
Alvaro Frias <alvarofriasgaray@gmail.com>
2020-07-11 17:05:52 -0300

    removed innecesary files and renamed

--- Lab3/Lab3.hs
+++ Lab3/Lab3.hs
@@ -31,42 +31,96 @@ data Expr a where
   CInt :: Int       -> Expr MInt                -- n
   V    :: Var       -> Expr MInt                -- v
   Plus :: Expr MInt -> Expr MInt -> Expr MInt   -- e + e'
+  Prod :: Expr MInt -> Expr MInt -> Expr MInt
+  Divs :: Expr MInt -> Expr MInt -> Expr MInt
+  Op :: Expr MInt -> Expr MInt 
+
   -- # Expresiones booleanas
+  CBool :: Bool -> Expr MBool
   Eq   :: Expr MInt  -> Expr MInt -> Expr MBool -- e = e'
   Lt   :: Expr MInt  -> Expr MInt -> Expr MBool -- e < e'
+  And  :: Expr MBool -> Expr MBool -> Expr MBool
+  Not  :: Expr MBool -> Expr MBool
+  Or :: Expr MBool -> Expr MBool -> Expr MBool
+
+
   -- # Comandos LIS
   Skip :: Expr Ω                                -- skip
-  -- v := e
-  -- c ; c'
-  -- if b then c else c'
-  -- newvar v := e in e'
-  -- while b do c
+  Assign :: Var -> Expr MInt -> Expr Ω               -- v := e
+  Seq :: Expr Ω -> Expr Ω -> Expr Ω                -- c ; c'
+  If :: Expr MBool -> Expr Ω -> Expr Ω -> Expr Ω                  -- if b then c else c'
+  Newvar :: Var -> Expr MInt -> Expr Ω -> Expr Ω             -- newvar v := e in e'
+  While :: Expr MBool -> Expr Ω -> Expr Ω                -- while b do c
   
   -- # Comandos Fallas
-  -- fail
-  -- catch c with c'
+  Fail :: Expr Ω-- fail
+  Catch :: Expr Ω -> Expr Ω -> Expr Ω-- catch c with c'
 
   -- # Comandos IO
-  -- !e
-  -- ?v
+  SOut :: Expr MInt -> Expr Ω -- !e
+  SIn :: Var -> Expr Ω-- ?v
 
 class DomSem dom where 
   sem :: Expr dom -> Σ -> dom
 
 instance DomSem MInt where
-  -- Completar
   sem (CInt a)     = \_ -> Just a
   sem (V v)        = \σ -> Just $ σ v
   sem (Plus e1 e2) = \σ -> ((+)-^-) (sem e1 σ) (sem e2 σ)
+  sem (Prod a b)   = \σ -> ((*)-^-) (sem a σ) (sem b σ)
+  sem (Divs a b)   = \σ -> if (sem b σ) == Just 0 then Nothing else ((div)-^-) (sem a σ) (sem b σ)
+  sem (Op a)       = \σ -> ((-)-^-) (Just 0) (sem a σ) 
+  
 
 instance DomSem MBool where
-  -- Completar
+  sem (CBool a)  = \_ -> Just a
+  sem (And a b)  = \σ -> ((&&)-^-) (sem a σ) (sem b σ)
+  sem (Not a)    = \σ -> ((not)-^) (sem a σ) 
   sem (Eq e1 e2) = \σ -> ((==)-^-) (sem e1 σ) (sem e2 σ)
   sem (Lt e1 e2) = \σ -> ((<)-^-) (sem e1 σ) (sem e2 σ)
+  sem (Or a b)   = \σ -> ((||)-^-) (sem a σ) (sem b σ)
 
 instance DomSem Ω where
-  -- Completar
+  --ecuaciones semánticas para LIS
   sem Skip = \σ -> Normal σ
+  sem (Assign v e) = \σ -> (update_var σ v (sem e σ))
+  sem (Seq c1 c2) = \σ -> (*.) (sem c2) (sem c1 σ) 
+  sem (If b c0 c1) = \σ -> if (sem b σ) == Nothing then Abort σ else if (sem b σ) == (Just True) then (sem c0 σ) else (sem c1 σ)
+  sem (Newvar v e c) = \σ -> (†.) (\s -> update s v (σ v)) (eval_newvar c σ v e)
+  sem (While b c) = \σ -> (fix (\w -> \σ -> if (sem b σ) == Nothing then (Abort σ) else if (sem b σ) == Just True then (*.) w (sem c σ) else (Normal σ))) σ 
+
+  -- ecuaciones semánticas para fallas
+  sem Fail = \σ -> Abort σ
+  sem (Catch c0 c1) = \σ -> (+.) (sem c1) (sem c0 σ)
+
+  --ecuaciones semánticas para IO
+  sem (SOut e) = \σ -> eval_sout (sem e σ) σ
+  sem (SIn v ) =  \σ -> In (\i -> Normal (update σ v i))
+
+
+-- función F de while
+--f :: (Σ -> Ω) -> (Σ -> Ω)
+--f = \w -> \σ -> if (sem b σ) then (*.) w (sem c σ) else (Normal σ)
+
+
+-- funciones auxiliares de ecuaciones semánticas
+unpack_mint :: Maybe Int -> Int
+unpack_mint (Just n) = n
+
+
+eval_newvar ::  Expr Ω -> Σ -> Var -> Expr MInt -> Ω
+eval_newvar c σ v e = if (sem e σ) == Nothing then Abort σ else (sem c (update σ v (unpack_mint (sem e σ))))
+
+
+update_var :: Σ -> Var -> Maybe Int  -> Ω
+update_var σ _ Nothing = Abort σ
+update_var σ v (Just n) = Normal (update σ v n)
+
+
+eval_sout :: Maybe Int -> Σ -> Ω
+eval_sout (Just n) σ = Out (n, Normal σ)
+eval_sout Nothing σ = Abort σ
+
 
 (>>==) :: (Maybe a, Σ) -> (a -> Ω) -> Ω
 (>>==) (Nothing, σ) _ = Abort σ
@@ -115,3 +169,134 @@ instance Eval Ω where
 
 (-^) :: (a -> b) -> (Maybe a -> Maybe b)
 (-^) = liftA
+
+
+
+-- state para probar
+state :: Σ
+state s | s == "z" = 12
+        | s == "x" = 0
+
+
+
+-- ejemplos para chequear las semánticas del lab 2
+-- sem (Divs (CInt 2) (Plus (V "x") (CInt  0))) state == Nothing
+-- sem (Divs (Op (Prod (CInt 2) (CInt 2))) (Plus (CInt 1) (CInt 1))) state == Just (-2)
+-- sem (Eq (Divs (Op (Prod (CInt 2) (CInt 2))) (Plus (CInt 1) (CInt 1))) (V "x")) state == Just False
+-- sem (Eq (V "x") (V "z")) state == Just False
+
+-- ejemplos LIS
+--  sem (Assign "x" (CInt 3)) state -> σx == 3
+--  sem (Seq (Assign "z" (CInt 3)) (Assign "x" (CInt 3))) state -> σx == 3 && σz == 3
+--  sem (If (Eq (V "x") (V "z")) (Assign "x" (CInt 3)) (Assign "z" (CInt 3))) state -> σz == 3 && σx == 0
+--  sem (Newvar "x" (CInt 1) (Assign "z" (V "x"))) state -> σz = 1 && σx == 0
+--  sem (Newvar "x" (Divs (CInt 2) (CInt 0)) (Skip)) state -> Abort 
+--  sem ( While (CBool True) (Assign "x" (CInt 1))) state -> ⊥
+--  sem (While (Lt (CInt 0) (Divs (CInt 1) (V "x"))) (Assign "x" (CInt 1))) state -> Abort
+--  sem (If (Eq (CInt 2)(Divs (CInt 1)(V "x"))) (Assign "x" (CInt 1)) (Skip)) state ->  Abort
+
+--ejemplos LIS + fallas
+--  sem (Fail) state -> Abort
+--  sem (Catch (Assign "x" (Divs (CInt 2) (CInt 0))) (Assign "x" (CInt 3))) state  -> σx == 3
+--  sem (Catch (Assign "x" (CInt 2)) (Assign "x" (CInt 0))) state-> σx == 2
+
+--ejemplos LIS + fallas + IO
+-- sem (SOut (CInt 2)) state
+-- sem (SOut (Divs (CInt 2) (V "x"))) state
+-- sem (SOut (V "x")) state
+-- sem (SIn (V "x")) state
+-- sem (Seq (SIn (V "x")) (SOut (V "x"))) state
+-- sem (While (CBool True) (SOut(V "x")) ) state
+
+
+-- funciones de testeo
+
+check_state_trans :: Σ -> Bool
+check_state_trans s = (s "x") == 2
+
+check_omega_ok :: Ω -> Bool
+check_omega_ok (Normal s) = check_state_trans s
+
+
+
+eval_in_ex :: IO ()
+eval_in_ex = eval (Seq (SIn "x") (SOut (V "x"))) state
+
+eval_out_ex :: IO ()
+eval_out_ex = eval (CInt 2) state
+
+
+check_abort :: Ω -> Bool
+check_abort (Abort e) = True
+check_abort x = False
+
+
+ej1 :: Expr Ω
+ej1 = While (Lt (V "x") (CInt 10)) $
+            Seq (SOut $ V "x")
+                (Assign "x" (Plus (V "x") (CInt 1)))
+
+eval_ej1 :: IO ()
+eval_ej1 = eval ej1 (\_ -> 0)
+
+
+ej2 :: Expr Ω
+ej2 = While (Lt (V "y") (CInt 10)) $
+            Seq (Seq (Seq (SIn "x")
+                          (SOut $ V "x")
+                     )
+                     (SOut $ V "y")
+                )
+                (Assign "y" (Plus (V "y") (CInt 1)))
+
+eval_ej2 :: IO ()
+eval_ej2 = eval ej2 (\_ -> 0)
+
+
+ej3 :: Expr Ω
+ej3 = Seq (Seq (SIn "x")
+               (Newvar "x" (CInt 10)
+                       (SOut $ V "x")
+               )
+          )
+          (SOut $ V "x")
+
+eval_ej3 :: IO ()
+eval_ej3 = eval ej3 (\_ -> 0)
+
+
+ej4 :: Expr Ω
+ej4 = While (CBool True) (SOut(V "x"))
+
+eval_ej4 :: IO ()
+eval_ej4 = eval ej4 (\_ -> 0)
+
+
+ej5 :: Expr Ω
+ej5 = (SOut (Divs (CInt 2) (V "x")))
+
+eval_ej5 :: IO ()
+eval_ej5 = eval ej5 (\_ -> 0)
+
+
+fibo :: Expr Ω
+fibo = (Seq (SIn "n")
+      $ Seq (Assign "x" (CInt 0))
+      $ Seq (Assign "z" (CInt 1))
+      $ Seq (If (Lt (V "n") (CInt 0)) (Fail) (Skip))
+      $ Seq (If (Eq (V "n") (CInt 0)) (Seq (SOut (V "x")) (Fail)) (Skip)) --corto programa con fail
+      $ Seq (If (Eq (V "n") (CInt 1)) (Seq (SOut (V "z")) (Fail)) (Skip))
+      $ Seq (Assign "i" (CInt 2))
+      $ Seq (While (Lt (V "i") (Plus (V "n") (CInt 1)))
+                  ( Seq (Assign "y" (Plus (V "x") (V "z")))
+                  $ Seq (Assign "x" (V "z"))
+                  $ Seq (Assign "z" (V "y"))
+                    (Assign "i" (Plus (V "i") (CInt 1)))
+                  ))
+      $ Seq (SOut (V "z"))
+      Skip
+      )
+
+
+eval_fibo :: IO ()
+eval_fibo = eval fibo (\_ -> 0)
